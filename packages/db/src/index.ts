a3a607d55fb4adf6064074175719c73a5c72c9e6
yash <yashgouravkar@gmail.com>
2026-02-22 09:12:34 +0530

    fixing tree shaking

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,7 @@
-import dotenv from "dotenv";
-
-dotenv.config({
-	path: "../../apps/server/.env",
-});
-
 import { drizzle } from "drizzle-orm/node-postgres";
-import { serverEnv } from "@milkpod/env/server";
 
-export const db = drizzle(serverEnv().DATABASE_URL);
+if (!process.env.DATABASE_URL) {
+	throw new Error("DATABASE_URL environment variable is not set");
+}
+
+export const db = drizzle(process.env.DATABASE_URL);
